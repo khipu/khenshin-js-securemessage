daa0f3b2e7e57923569f88df9963a304ef42923d
Julio Araya <julio.araya@khipu.com>
2020-08-19 13:07:24 -0400

    Fixed typescript definitions

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,12 @@
-interface Key {
-    raw: string
-    enc: string
+declare module 'khenshin-js-securemessage' {
+    export default class SecureMessage {
+        publicKey: string
+
+        static getInstance(): SecureMessage
+
+        encrypt(plainText: string, receiverPubKey: string, reuseExistingKey?: boolean): string
+
+        decrypt(cipherText: string, senderPublicKey: string, reuseExistingKey?: boolean): string
+    }
 }
 
-declare class SecureMessage {
-    publicKey: string
-
-    static getInstance(): SecureMessage
-
-    encrypt(plainText: string, receiverPubKey: string, reuseExistingKey: boolean): string
-
-    decrypt(cipherText: string, senderPublicKey: string, reuseExistingKey: boolean): string
-}
